605bc265fc1c399799c88190588a97b29bd49d78
Anthony Magee <amagee@synapsesoftware.com>
2015-03-20 14:23:07 -0600

    Added CATCH unit testing framework; sample edf file for tests; added new scheme to to Xcode build for unit tests

---
+++ EDFLib.h
@@ -0,0 +1,18 @@
+//
+//  EDFLib.h
+//  EDFLIB
+//
+//  Created by Anthony Magee on 3/16/15.
+//
+//
+
+#ifndef EDFLIB_h
+#define EDFLIB_h
+
+#include "EDFFile.h"
+#include "EDFUtil.h"
+#include "EDFHeader.h"
+#include "EDFAnnotation.h"
+#include "EDFSignalData.h"
+
+#endif
---
+++ Unit Tests/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Unit Tests
+//
+//  Created by Anthony Magee on 2/19/15.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
