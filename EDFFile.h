d7a50ca1e69273f8fae932e6ca71e6dc518c7d8d
Anthony Magee <amagee@synapsesoftware.com>
2015-03-20 14:35:18 -0600

    moved documentation to header; improved file handling; improved readability of getSignalData

--- EDFFile.h
+++ EDFFile.h
@@ -21,27 +21,54 @@
 #include "EDFAnnotation.h"
 #include "EDFSignalData.h"
 
-using std::fstream;
-using std::string;
-using std::vector;
 
 class EDFFile {
 public:
+    /**
+     Contructor to build new EDF file object. Access
+     to the data of an EDF file should start by instantiating
+     this class.
+     @param path Path to the EDF file on disk.
+     */
     EDFFile(const char*);
+    
+    /**
+     Destructor.
+     */
     virtual ~EDFFile();
 
+    /**
+     Get the header object associated with this file.
+     @return EDF header object.
+     */
     EDFHeader* getHeader();
-    vector<EDFAnnotation>* getAnnotations();
+    
+    /**
+     Get the annotations channel information vector.
+     @return EDF Annotation vector or nullptr if channel does not exist.
+     */
+    std::vector<EDFAnnotation>* getAnnotations();
+    
+    /**
+     Get a portion of a channel's signal information.
+     @param channel The channel to extract information from.
+     @param start The starting time in fractional seconds to
+     begin signal data extraction.
+     @param length The length of time in fractional seconds of
+     signal information to extract. If the length is greater than the
+     signal data, the time will be truncated to the max data length.
+     @return An EDFSignalData object containing the data extracted from file.
+     The following issues will cause a nullptr value to be returned:
+     Attempting to access the annotation channel.
+     Attempting to extract data from an nonexistent channel.
+     */
     EDFSignalData* getSignalData(int, double, double);
-    int getRealChannelCount();
 
 private:
-    string filePath;
-    fstream fileStream;
+    std::string filePath;
+    std::fstream fileStream;
     EDFHeader* header;
-    vector<EDFAnnotation>* annotation;
-
-    int realChannelCount;
+    std::vector<EDFAnnotation>* annotation;
 };
 
 #endif	/* _EDFFILE_H */
