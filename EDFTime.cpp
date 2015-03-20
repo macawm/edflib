554765af958b6f48dfd20ae59a7d41cb30967d0d
Anthony Magee <amagee@synapsesoftware.com>
2015-03-20 14:47:40 -0600

    improved C++11 compliance; improved default ctor; improved parsing algorithm

--- EDFTime.cpp
+++ EDFTime.cpp
@@ -10,6 +10,7 @@
 */
 
 #include "EDFTime.h"
+#include "EDFUtil.h"
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
@@ -17,59 +18,68 @@
 using std::cerr;
 using std::endl;
 
-/**
- Constructor to build new time object. Out of
- range values will be adjusted as appropriate.
- @param hour Integer value between 0 and 59.
- @param min Integer value between 0 and 59.
- @param sec Integer value greater than or equal to 0.
-*/
-EDFTime::EDFTime(int hour, int min, int sec) {
-    // validate sec
+void EDFTime::rectifyTime() {
     if (sec > 59) {
         min += sec / 60;
-        this->sec = sec % 60;
-    } else if (sec < 0)
-        this->sec = 0;
-    else
-        this->sec = sec;
-
-    // validate min
+        sec = sec % 60;
+    }
+    
     if (min > 59) {
         hour += min / 60;
-        this->min = min % 60;
-    } else if (min < 0)
-        this->min = 0;
-    else
+        min = min % 60;
+    }
+}
+
+EDFTime::EDFTime() : EDFTime(0, 0, 0) {}
+
+EDFTime::EDFTime(int hour, int min, int sec) {
+    if (sec >= 0) {
+        this->sec = sec;
+    }
+    
+    else {
+        this->sec = 0;
+        DI(cerr << "* RT  Could not set seconds to negative value: " << sec << endl)
+    }
+    
+    if (min >= 0) {
         this->min = min;
-
-    // validate hour
-    if (hour < 0)
-        this->hour = 0;
-    else
+    }
+    
+    else {
+        this->min = 0;
+        DI(cerr << "* RT  Could not set minutes to negative value: " << min << endl)
+    }
+    
+    if (hour >= 0) {
         this->hour = hour;
+    }
+    
+    else {
+        this->hour = 0;
+        DI(cerr << "* RT  Could not set hour to negative value: " << hour << endl)
+    }
+    
+    rectifyTime();
 }
 
-/**
- Constructor to build new time object from a positive number
- of seconds.
- @param seconds Integer greater than or equal to 0.
-*/
+
 EDFTime::EDFTime(int seconds) {
-    this->hour = seconds / 3600;
-    seconds %= 3600;
-    this->min = seconds / 60;
-    this->sec = seconds % 60;
+    if (seconds >= 0) {
+        this->hour = seconds / 3600;
+        seconds %= 3600;
+        this->min = seconds / 60;
+        this->sec = seconds % 60;
+    }
+    
+    else {
+        this->sec = this->min = this->hour = 0;
+        DI(cerr << "* RT  Could not set time to negative value: " << seconds << endl)
+    }
 }
 
-/**
- Constructor to parse time from string of required format
- "hh.mm.ss". Any format errors will cause the time to be
- set to 00.00.00.
- @param time String of aforementioned format.
-*/
+
 EDFTime::EDFTime(string time) {
-    int hour, min, sec;
     if (time[2] != '.' || time[5] != '.'  ||
         time[0] < '0' || time[0] > '9' ||
         time[1] < '0' || time[1] > '9' ||
@@ -77,58 +87,28 @@ EDFTime::EDFTime(string time) {
         time[4] < '0' || time[4] > '9' ||
         time[6] < '0' || time[6] > '9' ||
         time[7] < '0' || time[7] > '9')
-        hour = min = sec = 0;
+    {
+        this->sec = this->min = this->hour = 0;
+        DI(cerr << "* RT  Could not parse string: " << time << endl)
+    }
+    
     else {
-        hour = atoi(time.substr(0, 2).c_str());
-        min = atoi(time.substr(3, 2).c_str());
-        sec = atoi(time.substr(6, 2).c_str());
+        hour = a2i(time.substr(0, 2));
+        min  = a2i(time.substr(3, 2));
+        sec  = a2i(time.substr(6, 2));
     }
 
-    // validate sec
-    if (sec > 59) {
-        min += sec / 60;
-        this->sec = sec % 60;
-    } else if (sec < 0)
-        this->sec = 0;
-    else
-        this->sec = sec;
-
-    // validate min
-    if (min > 59) {
-        hour += min / 60;
-        this->min = min % 60;
-    } else if (min < 0)
-        this->min = 0;
-    else
-        this->min = min;
-
-    // validate hour
-    if (hour < 0)
-        this->hour = 0;
-    else
-        this->hour = hour;
+    rectifyTime();
 }
 
-/**
- Copy constructor.
- @param orig The object to copy.
-*/
+
 EDFTime::EDFTime(const EDFTime& orig) {
     hour = orig.hour;
     min = orig.min;
     sec = orig.sec;
 }
 
-/**
- Destructor.
-*/
-EDFTime::~EDFTime() {
-}
 
-/**
- Operator = overload to make a deep copy.
- @param rhs The object to copy.
-*/
 EDFTime& EDFTime::operator=(const EDFTime& rhs) {
     if (this != &rhs) {
         hour = rhs.hour;
@@ -139,12 +119,7 @@ EDFTime& EDFTime::operator=(const EDFTime& rhs) {
     return *this;
 }
 
-/**
- Output operator to make a readable string of this object.
- @param s An output stream reference to place the data into.
- @param time The time to stringify.
- @return The output stream.
-*/
+
 std::ostream& operator<<(std::ostream& s, EDFTime time) {
     s << std::setw(2) << std::setfill('0') << time.hour << ".";
     s << std::setw(2) << std::setfill('0') << time.min << ".";
@@ -152,78 +127,50 @@ std::ostream& operator<<(std::ostream& s, EDFTime time) {
     return s;
 }
 
-/**
- Get the integer hour of this object.
- @return Integer in [0, maxInt].
-*/
+
 int EDFTime::getHour() { return hour; }
 
-/**
- Get the integer minute of this object.
- @return Integer in [0, 59].
-*/
 int EDFTime::getMin() { return min; }
 
-/**
- Get the integer second of this object.
- @return Integer in [0, 59].
-*/
 int EDFTime::getSec() { return sec; }
 
-/**
- Set the time's hour value. Out of range values
- will default to 0.
- @param day Integer value between 0 and maxInt.
-*/
+
 void EDFTime::setHour(int hour) {
-    // validate hour
-    if (hour < 0)
-        this->hour = 0;
-    else
+    if (hour >= 0) {
         this->hour = hour;
-}
+        // no need to rectify here
+    }
 
-/**
- Set the time's minute value. Out of range values
- will default to 0.
- @param day Integer value between 0 and 59.
-*/
-void EDFTime::setMin(int min) {
-    // validate min
-    if (min < 0)
-        this->min = 0;
-    else if (min > 59) {
-        this->hour += min / 60;
-        this->min = min % 60;
-    } else
-        this->min = min;
-}
-
-/**
- Set the time's second value. Out of range values
- will default to 0.
- @param day Integer value between 0 and 59.
-*/
-void EDFTime::setSec(int sec) {
-    // validate sec
-    if (sec < 0)
-        this->sec = 0;
-    else if (sec > 59) {
-        this->min += sec / 60;
-        this->sec = sec % 60;
-    } else
-        this->sec = sec;
-
-    if (min > 59) {
-        this->hour += this->min / 60;
-        this->min %= 60;
+    else {
+        DI(cerr << "* RT  Could not set hours to negative value: " << hour << endl)
     }
 }
 
-/**
- Get the value of this time object in seconds.
- @return Integer in [0, maxInt]
-*/
+
+void EDFTime::setMin(int min) {
+    if (min >= 0) {
+        this->min = min;
+        rectifyTime();
+    }
+
+    else {
+        DI(cerr << "* RT  Could not set minutes to negative value: " << min << endl)
+    }
+}
+
+
+void EDFTime::setSec(int sec) {
+    if (sec >= 0) {
+        this->sec = sec;
+        rectifyTime();
+    }
+    
+    else {
+        DI(cerr << "* RT  Could not set seconds to negative value: " << sec << endl)
+    }
+}
+
+
 int EDFTime::asSeconds() {
     return hour * 3600 + min * 60 + sec;
 }
--- EDFTime.h
+++ EDFTime.h
@@ -13,32 +13,111 @@
 
 #include <string>
 #include <ostream>
+#include <exception>
 
 using std::string;
 
 class EDFTime {
 public:
-    EDFTime() {};
+    EDFTime();
+    
+    /**
+     Constructor to parse time from string of required format
+     "hh.mm.ss". Any format errors will cause the time to be
+     set to 00.00.00.
+     @param time String of aforementioned format.
+     */
     EDFTime(string);
+    
+    /**
+     Constructor to build new time object from a positive number
+     of seconds.
+     @param seconds Integer greater than or equal to 0.
+     */
     EDFTime(int);
+    
+    /**
+     Constructor to build new time object. Out of
+     range values will be adjusted as appropriate.
+     @param hour Integer value between 0 and 59.
+     @param min Integer value between 0 and 59.
+     @param sec Integer value greater than or equal to 0.
+     */
     EDFTime(int, int, int);
+    
+    /**
+     Copy constructor.
+     @param orig The object to copy.
+     */
     EDFTime(const EDFTime&);
-    virtual ~EDFTime();
+    
+    /**
+     Destructor.
+     */
+    virtual ~EDFTime() {};
 
+    /**
+     Operator = overload to make a deep copy.
+     @param rhs The object to copy.
+     */
     EDFTime& operator=(const EDFTime&);
+    
+    /**
+     Output operator to make a readable string of this object.
+     @param s An output stream reference to place the data into.
+     @param time The time to stringify.
+     @return The output stream.
+     */
     friend std::ostream& operator<<(std::ostream&, EDFTime);
 
+    /**
+     Get the integer hour of this object.
+     @return Integer in [0, maxInt].
+     */
     int getHour();
+    
+    /**
+     Get the integer minute of this object.
+     @return Integer in [0, 59].
+     */
     int getMin();
+    
+    /**
+     Get the integer second of this object.
+     @return Integer in [0, 59].
+     */
     int getSec();
 
+    /**
+     Set the time's hour value.
+     @param day Integer value between 0 and maxInt. (Values below 0 are ignored.)
+     */
     void setHour(int);
+    
+    /**
+     Set the time's minute value.
+     @param day Integer value between 0 and 59. (Values below 0 are ignored and values over 59 are rectified as hours and minutes.)
+     */
     void setMin(int);
+    
+    /**
+     Set the time's second value.
+     @param day Integer value between 0 and 59.
+     */
     void setSec(int);
 
+    /**
+     Get the value of this time object in seconds.
+     @return Integer in [0, maxInt]
+     */
     int asSeconds();
+    
+    
 private:
     int hour, min, sec;
+    
+protected:
+    void rectifyTime();
 };
 
 #endif	/* _EDFTIME_H */
